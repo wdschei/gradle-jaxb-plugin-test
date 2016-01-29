a487a46f2b9318c03a1e81c643fa2863e969364e
wdschei <Bill.Scheidegger@Rackspace.com>
2016-01-29 11:42:37 -0600

    Adding the first go at the source for the test.

---
+++ sub-proj-morty/src/main/scala/org/openrepose/jaxbtestmorty/jaxbTestMorty.scala
@@ -0,0 +1,7 @@
+package org.openrepose.jaxbtestmorty
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+
+class jaxbTestMorty () extends LazyLogging {
+  override def toString: String = { "Morty!!!" }
+}
---
+++ sub-proj-rick/src/main/scala/org/openrepose/jaxbtestrick/jaxbTestRick.scala
@@ -0,0 +1,7 @@
+package org.openrepose.jaxbtestrick
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+
+class jaxbTestRick () extends LazyLogging {
+  override def toString: String = { "Rick!!!" }
+}
